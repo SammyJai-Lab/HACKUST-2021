49ee9eb0a393281888915a64678ee87e4d00e3ef
Juke <68906708+Jukeeeee@users.noreply.github.com>
2021-04-14 02:55:48 +0800

    Update node.ino

--- NodeFirmware/node/node.ino
+++ NodeFirmware/node/node.ino
@@ -1,21 +1,40 @@
-//FirebaseESP8266.h must be included before ESP8266WiFi.h
-#include "FirebaseESP8266.h"	// Install Firebase ESP8266 library
+#include "FirebaseESP8266.h" //Including firebase library to enable the ability for the node to connect to the firebase
 
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h> //ESP8266WiFi.h from "http://arduino.esp8266.com/stable/package_esp8266com_index.json"
+
+//Libraries for MFRC522 reader
+#include <SPI.h>
+#include <MFRC522.h>
 
 
-#define FIREBASE_HOST "fir-iot-85fdf-default-rtdb.firebaseio.com/" //Without http:// or https:// schemes
+#define FIREBASE_HOST "fir-iot-85fdf-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "sG8YYCucVC9Xjd0503QDNCJybTLKBAFPpANTTwN0"
 #define WIFI_SSID "printer"
 #define WIFI_PASSWORD "12345678"
 
-int BUTPIN = D1;
-int led = D5; // Connect LED to D5
+const int PRESS_THRESHOLD = 5000;
 
-int buttonState = 0;
-int buttonCount = 0;
+constexpr uint8_t RST_PIN = D3;
+constexpr uint8_t SSR_PIN = D4;
 
-//Define FirebaseESP8266 data object
+MFRC522 rfid(SSR_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+
+String tag;
+
+int button = D1;
+int ledRed = D8;
+int ledGreen = D2;
+
+int buttonState = 0; //State of button on the main module. 1 = pressed - 0 = not pressed
+int ledRedState = 0; //State of red led on the main module. 1 = Lights on - 0 = Lights off
+int ledGreenState = 0; //State of green led on the main module. 1 = Lights on - 0 = Lights off
+
+int buttonLastState = LOW;
+int buttonCurrentState;
+
+bool serveState = false;
+
 FirebaseData firebaseData;
 FirebaseData ledData;
 
@@ -25,15 +44,23 @@ void setup() {
 
     Serial.begin(9600);
 
-    pinMode(BUTPIN, INPUT);
-    pinMode(led, OUTPUT);
+    SPI.begin();
+    rfid.PCD_Init();
+
+    pinMode(button, INPUT);
+    pinMode(ledRed, OUTPUT);
+    pinMode(ledGreen, OUTPUT);
 
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting to Wi-Fi");
+    int timer = 0;
     while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
+        Serial.print(".Connecting to wifi --timer: ");
+        Serial.println(timer);
+        timer += 300;
         delay(300);
     }
+
     Serial.println();
     Serial.print("Connected with IP: ");
     Serial.println(WiFi.localIP());
@@ -44,14 +71,8 @@ void setup() {
 
 }
 
-void sensorUpdate() {
-    
-    buttonState = digitalRead(BUTPIN);
-    if (buttonState == HIGH) {
-        buttonCount++;
-    }
-
-    if (Firebase.setInt(firebaseData, "/FirebaseIOT/buttonState", buttonState)) {
+void shortPress() {
+    if (Firebase.setInt(firebaseData, "/FirebaseIOT/serveState", 1)) {
         Serial.println("PASSED");
         Serial.println("PATH: " + firebaseData.dataPath());
         Serial.println("TYPE: " + firebaseData.dataType());
@@ -65,27 +86,13 @@ void sensorUpdate() {
         Serial.println();
     }
 
-    if (Firebase.setInt(firebaseData, "/FirebaseIOT/buttonCount", buttonCount)) {
+    if (Firebase.setString(ledData, "/FirebaseIOT/ledRed", "1")) {
         Serial.println("PASSED");
-        Serial.println("PATH: " + firebaseData.dataPath());
-        Serial.println("TYPE: " + firebaseData.dataType());
-        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("PATH: " + ledData.dataPath());
+        Serial.println("TYPE: " + ledData.dataType());
+        Serial.println("ETag: " + ledData.ETag());
         Serial.println("------------------------------------");
         Serial.println();
-    } else {
-        Serial.println("FAILED");
-        Serial.println("REASON: " + firebaseData.errorReason());
-        Serial.println("------------------------------------");
-        Serial.println();
-    }
-
-    if (Firebase.getString(ledData, "/FirebaseIOT/led")) {
-        Serial.println(ledData.stringData());
-        if (ledData.stringData() == "1") {
-            digitalWrite(led, HIGH);
-        } else if (ledData.stringData() == "0") {
-            digitalWrite(led, LOW);
-        }
     } else {
         Serial.println("FAILED");
         Serial.println("REASON: " + ledData.errorReason());
@@ -93,11 +100,174 @@ void sensorUpdate() {
         Serial.println();
     }
 
+    if (Firebase.setInt(firebaseData, "/FirebaseIOT/buttonCount", Firebase.getInt(firebaseData, "/FirebaseIOT/buttonCount") + 1)) {
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    } else {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+}
+
+void longPress() {
+    //Reset everything
+    if (Firebase.setInt(firebaseData, "/FirebaseIOT/serveState", 0)) {
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    } else {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+
+    if (Firebase.setInt(firebaseData, "/FirebaseIOT/buttonCount", 0)) {
+        Serial.println("PASSED");
+        Serial.println("PATH: " + firebaseData.dataPath());
+        Serial.println("TYPE: " + firebaseData.dataType());
+        Serial.println("ETag: " + firebaseData.ETag());
+        Serial.println("------------------------------------");
+        Serial.println();
+    } else {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + firebaseData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+}
+
+void buttonUpdate() {
+    unsigned long pressedTime  = 0;
+    unsigned long releasedTime = 0;
+    buttonCurrentState = digitalRead(button);
+    if(buttonLastState == HIGH && buttonCurrentState == LOW)
+        pressedTime = millis();
+    else if(buttonLastState == LOW && buttonCurrentState == HIGH) {
+        releasedTime = millis();
+        long pressDuration = releasedTime - pressedTime;
+        if(pressDuration < PRESS_THRESHOLD)
+            shortPress();
+        else
+            longPress();
+    }
+    buttonLastState = buttonCurrentState;
+}
+
+
+void ledUpdate() {
+    if (Firebase.getString(ledData, "/FirebaseIOT/ledRed")) {
+        Serial.println(ledData.stringData());
+        if (ledData.stringData() == "1") {
+            digitalWrite(ledRed, HIGH);
+        } else if (ledData.stringData() == "0") {
+            digitalWrite(ledRed, LOW);
+        }
+        Serial.println("LED RED DATA FETCH SUCCESS");
+        Serial.println("LED STATE: " + ledData.stringData());
+        Serial.println("------------------------------------");
+        Serial.println();
+    } else {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + ledData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+
+    if (Firebase.getString(ledData, "/FirebaseIOT/ledGreen")) {
+        Serial.println(ledData.stringData());
+        if (ledData.stringData() == "1") {
+            digitalWrite(ledGreen, HIGH);
+        } else if (ledData.stringData() == "0") {
+            digitalWrite(ledGreen, LOW);
+        }
+        Serial.println("LED GREEN DATA FETCH SUCCESS");
+        Serial.println("LED STATE: " + ledData.stringData());
+        Serial.println("------------------------------------");
+        Serial.println();
+    } else {
+        Serial.println("LED GREEN DATA FETCH FAILED");
+        Serial.println("REASON: " + ledData.errorReason());
+        Serial.println("------------------------------------");
+        Serial.println();
+    }
+
+}
+
+void rfidSensorUpdate() {
+    if ( ! rfid.PICC_IsNewCardPresent())
+        return;
+    if (rfid.PICC_ReadCardSerial()) {
+        for (byte i = 0; i < 4; i++) {
+            tag += rfid.uid.uidByte[i];
+        }
+        Serial.println(tag);
+        tag = "";
+        rfid.PICC_HaltA();
+        rfid.PCD_StopCrypto1();
+
+        if (Firebase.getInt(firebaseData, "/FirebaseIOT/serveState")) {
+            Serial.println("PASSED");
+        }
+        if (firebaseData.intData() == 1) {
+            if (Firebase.setInt(firebaseData, "/FirebaseIOT/serveState", 0)) {
+                Serial.println("PASSED");
+                Serial.println("PATH: " + firebaseData.dataPath());
+                Serial.println("TYPE: " + firebaseData.dataType());
+                Serial.println("ETag: " + firebaseData.ETag());
+                Serial.println("------------------------------------");
+                Serial.println();
+            } else {
+                Serial.println("FAILED");
+                Serial.println("REASON: " + firebaseData.errorReason());
+                Serial.println("------------------------------------");
+                Serial.println();
+            }
+
+            if (Firebase.setString(ledData, "/FirebaseIOT/ledRed", "0")) {
+                Serial.println("PASSED");
+                Serial.println("PATH: " + ledData.dataPath());
+                Serial.println("TYPE: " + ledData.dataType());
+                Serial.println("ETag: " + ledData.ETag());
+                Serial.println("------------------------------------");
+                Serial.println();
+            } else {
+                Serial.println("FAILED");
+                Serial.println("REASON: " + ledData.errorReason());
+                Serial.println("------------------------------------");
+                Serial.println();
+            }
+        }
+
+        if (Firebase.pushString(firebaseData, "/FirebaseIOT/serveHistory", tag)) {
+            Serial.println("PASSED");
+            Serial.println("------------------------------------");
+            Serial.println();
+        } else {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + firebaseData.errorReason());
+            Serial.println("------------------------------------");
+            Serial.println();
+        }
+    }
+}
+
+void stateUpdate() {
+    int x = 1;
 }
 
 void loop() {
-    sensorUpdate();
-
-
-    delay(10);
+    buttonUpdate();
+    ledUpdate();
+    stateUpdate();
+    
 }
